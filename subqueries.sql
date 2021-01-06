b546710b4968b7bb8dbc75958ac2e6810c725ea2
John Grinstead <john.allen.grinstead@gmail.com>
2021-01-06 11:42:26 -0600

    completed subqueries exercise

--- subqueries.sql
+++ subqueries.sql
@@ -25,13 +25,15 @@ group by title;
 
 -- How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 
-select * 
+select count(emp_no)
 from employees
-join dept_emp using (emp_no)
-where dept_emp.to_date < curdate()
-order by emp_no;
+where emp_no not in (
+	select emp_no
+	from salaries
+	where to_date > curdate()
+);
 
--- 91479 employees no longer with the company
+-- 59900 employees no longer with the company
 
 -- Find all the current department managers that are female. List their names in a comment in your code.
 
@@ -60,6 +62,23 @@ order by salary;
 select avg(salary) -- used to find avg salary 
 from salaries;
 
+-- How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+
+select
+ (Select count(salary)
+ from salaries
+ where salary > (
+        Select 
+        max(salary) - STDDEV(salary)
+        from salaries
+        Where to_date > curdate())
+ AND to_date > curdate())
+ /
+ (select
+ count(salary)
+ from salaries
+ Where to_date > curdate())
+ *100;
 
 
 -- BONUS!!!!!!!!!!!!!!!!
@@ -77,6 +96,35 @@ where dept_no in (
 		select emp_no
 		from employees
 		where gender = 'F'
+	)
+	and to_date > curdate()
+);
+
+-- Find the first and last name of the employee with the highest salary.
+
+select first_name, last_name
+from employees
+where emp_no in (
+	select emp_no
+	from salaries
+	where salary in(
+		select max(salary)
+		from salaries
+	)
+	and to_date > curdate()
+);
+
+-- Find the department name that the employee with the highest salary works in.
+
+select employees.first_name, employees.last_name, dept_name
+from employees_with_departments
+join employees using (emp_no)
+where emp_no in (
+	select emp_no
+	from salaries
+	where salary in(
+		select max(salary)
+		from salaries
 	)
 	and to_date > curdate()
 );
